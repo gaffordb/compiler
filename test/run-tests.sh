628b2f84d60eb83b0ba1e8582f3f551b19e96e34
gaffordb <gaffordb@grinnell.edu>
2018-01-28 14:10:13 -0600

    updated testing

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #this only works if i'm in the correct directory... howtodothisnotshitty?
 echo Running test 1...
-../bin/program foo bar baz > tmp.txt
-OUT=$(diff tmp.txt test1.out)
 
+./bin/program foo bar baz > tmp.txt
+OUT=$(diff tmp.txt test/test1.out)
+rm tmp.txt
 if [ ${#OUT} == 0 ]; then
     echo Test successful.
+    exit 0
 else
     echo Code broken!
+    exit 1
 fi
-
-rm tmp.txt
