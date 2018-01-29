949bd2528d358409b654c69ed4dfb569177e8a27
gaffordb <gaffordb@grinnell.edu>
2018-01-29 14:48:37 -0600

    cleaned up project

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#this only works if i'm in the correct directory... howtodothisnotshitty?
-echo Running test 1...
-
+echo Running test 1... 
 ./bin/program foo bar baz > tmp.txt
+
 OUT=$(diff tmp.txt test/test1.out)
 rm tmp.txt
 if [ ${#OUT} == 0 ]; then
